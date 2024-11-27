31017fb005d0fad504bb62fa3df188c490f0f37a
Mohcinelahfari <lahfarimohcine01@gmail.com>
2024-11-27 13:47:25 +0100

    Updated SearchBar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,23 +1,42 @@
 import {Button, Form} from "react-bootstrap";
 import styles from  './SearchBar.module.scss'
 import { Autocomplete, TextField } from "@mui/material";
+import { useState } from "react";
 export const SearchBar = () => {
+    const [cities, setCities] = useState([])
     const GEO_API_KEY =  process.env.REACT_APP_GEO_API_KEY
+    const WHEATHER_API_KEY = process.env.REACT_APP_WEATHER_API
+    console.log(WHEATHER_API_KEY)
     const handelchange = (e) => {
     
         const {value} = e.currentTarget
         console.log(GEO_API_KEY)
-        fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=Paris&apiKey=de7a1b4e35e74e31ba4e370bf210ba4c`)
+        fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${value}&apiKey=${GEO_API_KEY}`)
         .then((response) => response.json())
-        .then((json) => console.log(json))
-        console.log(value)
+        .then((json) => setCities(json.features.map(data => {
+            const { city, country, state, formatted } = data.properties;  // properties contain city, country, etc.
+            const [lon, lat] = data.geometry.coordinates; 
+            return {city, country, state, lat, lon,formatted}
+        }
+        )))
     }
+
+    const handelAutoCompliteChange = (e, value) => {
+        const { lat, lon } = value;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WHEATHER_API_KEY}`)
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+            .catch((error) => console.error("Error fetching weather data:", error));
+    };
+    
     return (  
         <>
             <Form>
                 <Form.Group className={styles.searchContainer}>
-                <Autocomplete className={styles.searchInput} renderInput={(params) => <TextField
-                 {...params} onChange={handelchange} label='Enter your city ...' />} options={['CasaBlanca','Rabat','fes']} />
+                <Autocomplete  className={styles.searchInput} clearOnBlur={false} getOptionLabel={(option) => option.formatted}
+                    onChange={handelAutoCompliteChange}
+                renderInput={(params) => <TextField
+                 {...params} onChange={handelchange} label='Enter your city ...' />} options={cities} />
                     <Button size={'sm'} variant='primary'>Search</Button>
                 </Form.Group>
             </Form>
